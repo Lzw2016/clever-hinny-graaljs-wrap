c0579546ac5f876cd8b1730639a0eea205b2ccf4
lizw <1183409807@qq.com>
2020-08-09 14:43:30 +0800

    add T03JClassTest.java add T04JDateTest.java

--- graaljs-test/src/test/java/org/clever/hinny/graal/test/base/T03JClassTest.java
+++ graaljs-test/src/test/java/org/clever/hinny/graal/test/base/T03JClassTest.java
@@ -6,25 +6,19 @@ import org.junit.Test;
 
 /**
  * 作者：lizw <br/>
- * 创建时间：2020/07/31 21:52 <br/>
+ * 创建时间：2020/08/09 14:24 <br/>
  */
 @Slf4j
-public class T01JObjectTest extends AbstractJdbcDataSourceTest {
+public class T03JClassTest extends AbstractJdbcDataSourceTest {
     @Before
     public void before() throws Exception {
         super.before();
-        scriptObject = engineInstance.require("/test/dist/base/01JObject");
+        scriptObject = engineInstance.require("/test/dist/base/03JClass");
     }
 
     @Test
     public void t01() {
         scriptObject.callMember("t01");
-        log.info("--------------------------------------------------------------------------------------------");
-    }
-
-    @Test
-    public void t02() {
-        scriptObject.callMember("t02");
         log.info("--------------------------------------------------------------------------------------------");
     }
 }
