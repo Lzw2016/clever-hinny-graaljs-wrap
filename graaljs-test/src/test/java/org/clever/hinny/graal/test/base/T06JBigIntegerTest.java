44e4232d66634c43678fc98fab6e6dfd1a5783f9
ymx <mlrsyz@vip.qq.com>
2020-08-10 16:32:24 +0800

    JBigInteger测试

--- graaljs-test/src/test/java/org/clever/hinny/graal/test/base/T06JBigIntegerTest.java
+++ graaljs-test/src/test/java/org/clever/hinny/graal/test/base/T06JBigIntegerTest.java
@@ -1,8 +1,24 @@
 package org.clever.hinny.graal.test.base;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * 作者：lizw <br/>
  * 创建时间：2020/08/09 14:49 <br/>
  */
-public class T06JBigIntegerTest {
+@Slf4j
+public class T06JBigIntegerTest extends AbstractJdbcDataSourceTest {
+    @Before
+    public void before() throws Exception {
+        super.before();
+        scriptObject = engineInstance.require("/test/dist/base/06JBigInteger");
+    }
+
+    @Test
+    public void t01(){
+        scriptObject.callMember("t01");
+        log.info("### ---------------------------------------------------------------------------> END");
+    }
 }
