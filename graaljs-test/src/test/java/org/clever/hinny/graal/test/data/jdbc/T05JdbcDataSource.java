794fbec65cc52c0225d946e7cf5b414af6fadd1e
ymx <mlrsyz@vip.qq.com>
2020-08-10 12:49:39 +0800

    测试

--- graaljs-test/src/test/java/org/clever/hinny/graal/test/data/jdbc/T05JdbcDataSource.java
+++ graaljs-test/src/test/java/org/clever/hinny/graal/test/data/jdbc/T05JdbcDataSource.java
@@ -21,4 +21,10 @@ public class T05JdbcDataSource extends AbstractJdbcDataSourceTest {
         scriptObject.callMember("t01");
         log.info("### ---------------------------------------------------------------------------> END");
     }
+
+    @Test
+    public void t02() {
+        scriptObject.callMember("t02");
+        log.info("### ---------------------------------------------------------------------------> END");
+    }
 }
