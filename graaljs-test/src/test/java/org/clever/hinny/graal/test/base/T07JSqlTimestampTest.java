f8423fd08ebee928cfca640779b51f510b4e0640
ymx <mlrsyz@vip.qq.com>
2020-08-14 13:58:17 +0800

    测试

--- graaljs-test/src/test/java/org/clever/hinny/graal/test/base/T07JSqlTimestampTest.java
+++ graaljs-test/src/test/java/org/clever/hinny/graal/test/base/T07JSqlTimestampTest.java
@@ -1,8 +1,24 @@
 package org.clever.hinny.graal.test.base;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * 作者：lizw <br/>
  * 创建时间：2020/08/09 14:49 <br/>
  */
-public class T07JSqlTimestampTest {
+@Slf4j
+public class T07JSqlTimestampTest extends AbstractJdbcDataSourceTest {
+    @Before
+    public void before() throws Exception {
+        super.before();
+        scriptObject = engineInstance.require("/test/dist/base/07JSqlTimestamp");
+    }
+
+    @Test
+    public void t01() {
+        scriptObject.callMember("t01");
+        log.info("### ---------------------------------------------------------------------------> END");
+    }
 }
