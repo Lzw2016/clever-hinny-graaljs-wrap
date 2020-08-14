c6afd9a981275d3ddfc4acc0250eb0dd9d7d608c
ymx <mlrsyz@vip.qq.com>
2020-08-14 16:26:48 +0800

    测试

--- graaljs-test/src/test/java/org/clever/hinny/graal/test/base/T09JSqlDateTest.java
+++ graaljs-test/src/test/java/org/clever/hinny/graal/test/base/T09JSqlDateTest.java
@@ -1,8 +1,24 @@
 package org.clever.hinny.graal.test.base;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * 作者：lizw <br/>
  * 创建时间：2020/08/09 14:50 <br/>
  */
-public class T09JSqlDateTest {
+@Slf4j
+public class T09JSqlDateTest  extends AbstractJdbcDataSourceTest {
+    @Before
+    public void before() throws Exception {
+        super.before();
+        scriptObject = engineInstance.require("/test/dist/base/09JSqlDate");
+    }
+
+    @Test
+    public void t01() {
+        scriptObject.callMember("t01");
+        log.info("### ---------------------------------------------------------------------------> END");
+    }
 }
