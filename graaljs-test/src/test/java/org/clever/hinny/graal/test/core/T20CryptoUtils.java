ee3d8b068b2359d23ab754be14ed97b29f073ca9
ymx <mlrsyz@vip.qq.com>
2020-08-24 16:51:31 +0800

    测试

---
+++ graaljs-test/src/test/java/org/clever/hinny/graal/test/core/T20CryptoUtils.java
@@ -0,0 +1,20 @@
+package org.clever.hinny.graal.test.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+
+@Slf4j
+public class T20CryptoUtils extends AbstractTest {
+    @Before
+    public void before() throws Exception {
+        super.before();
+        scriptObject = engineInstance.require("/test/dist/core/20CryptoUtils");
+    }
+
+    @Test
+    public void t01() {
+        scriptObject.callMember("t01");
+        log.info("### ---------------------------------------------------------------------------> END");
+    }
+}
