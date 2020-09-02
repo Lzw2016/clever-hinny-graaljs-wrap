eb4adfb7174e2761d2912ff69e209658d645711a
lizw <1183409807@qq.com>
2020-09-02 21:31:28 +0800

    动态SQL 75%

--- graaljs-data-jdbc/src/main/java/org/clever/hinny/graal/data/jdbc/MyBatisJdbcDatabase.java
+++ graaljs-data-jdbc/src/main/java/org/clever/hinny/graal/data/jdbc/MyBatisJdbcDatabase.java
@@ -1,5 +1,11 @@
 package org.clever.hinny.graal.data.jdbc;
 
+import lombok.SneakyThrows;
+import org.clever.hinny.data.jdbc.support.JdbcDataSourceStatus;
+import org.clever.hinny.data.jdbc.support.JdbcInfo;
+import org.springframework.util.Assert;
+
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -17,4 +23,170 @@ public class MyBatisJdbcDatabase {
     protected MyBatisJdbcDatabase() {
     }
 
+    /**
+     * 设置默认数据源
+     *
+     * @param defaultName 默认数据源名称
+     * @return 默认数据源对象
+     */
+    public MyBatisJdbcDataSource setDefault(String defaultName) {
+        Assert.hasText(defaultName, "参数defaultName不能为空");
+        Assert.isTrue(DATASOURCE_MAP.containsKey(defaultName) && !DATASOURCE_MAP.get(defaultName).isClosed(), "默认数据源已经关闭(isClosed)");
+        this.defaultName = defaultName;
+        return getDefault();
+    }
+
+    /**
+     * 获取默认数据源
+     */
+    public MyBatisJdbcDataSource getDefault() {
+        return getDataSource(defaultName);
+    }
+
+    /**
+     * 获取默认数据源名称
+     */
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    /**
+     * 根据名称获取数据源
+     *
+     * @param name 数据源名称
+     */
+    public MyBatisJdbcDataSource getDataSource(String name) {
+        return DATASOURCE_MAP.get(name);
+    }
+
+    /**
+     * 判断数据源是否存在
+     *
+     * @param name 数据源名称
+     */
+    public boolean hasDataSource(String name) {
+        return DATASOURCE_MAP.containsKey(name);
+    }
+
+    /**
+     * 添加数据源
+     *
+     * @param name                  数据源名称
+     * @param myBatisJdbcDataSource MyBatis数据源
+     */
+    public void add(String name, org.clever.hinny.data.jdbc.MyBatisJdbcDataSource myBatisJdbcDataSource) {
+        Assert.notNull(myBatisJdbcDataSource, "参数myBatisJdbcDataSource不能为空");
+        Assert.isTrue(!DATASOURCE_MAP.containsKey(name), "数据源已经存在");
+        DATASOURCE_MAP.put(name, new MyBatisJdbcDataSource(myBatisJdbcDataSource));
+    }
+
+//    /**
+//     * 添加数据源
+//     *
+//     * @param name       数据源名称
+//     * @param jdbcConfig 数据源配置
+//     */
+//    @SuppressWarnings("unchecked")
+//    public MyBatisJdbcDataSource add(String name, Map<String, Object> jdbcConfig) {
+//        Assert.isTrue(!DATASOURCE_MAP.containsKey(name), "数据源已经存在");
+//        jdbcConfig = InteropScriptToJavaUtils.Instance.convertMap(jdbcConfig);
+//        JdbcConfig config = new JdbcConfig();
+//        config.setDriverClassName((String) jdbcConfig.get("driverClassName"));
+//        config.setJdbcUrl((String) jdbcConfig.get("jdbcUrl"));
+//        config.setUsername((String) jdbcConfig.get("username"));
+//        config.setPassword((String) jdbcConfig.get("password"));
+//        config.setAutoCommit((Boolean) jdbcConfig.get("autoCommit"));
+//        config.setReadOnly((Boolean) jdbcConfig.get("readOnly"));
+//        config.setMaxPoolSize((Integer) jdbcConfig.get("maxPoolSize"));
+//        config.setMinIdle((Integer) jdbcConfig.get("minIdle"));
+//        config.setMaxLifetimeMs((Long) jdbcConfig.get("maxLifetimeMs"));
+//        config.setConnectionTimeoutMs((Long) jdbcConfig.get("connectionTimeoutMs"));
+//        config.setIdleTimeoutMs((Long) jdbcConfig.get("idleTimeoutMs"));
+//        config.setConnectionTestQuery((String) jdbcConfig.get("connectionTestQuery"));
+//        config.setDataSourceProperties((Map<String, Object>) jdbcConfig.get("dataSourceProperties"));
+//        org.clever.hinny.data.jdbc.JdbcDataSource jdbcDataSource = new org.clever.hinny.data.jdbc.JdbcDataSource(config.getHikariConfig());
+//        add(name, jdbcDataSource);
+//        return DATASOURCE_MAP.get(name);
+//    }
+
+    /**
+     * 删除数据源
+     *
+     * @param name 数据源名称
+     */
+    @SneakyThrows
+    public boolean del(String name) {
+        Assert.isTrue(!Objects.equals(name, defaultName), "不能删除默认数据源");
+        MyBatisJdbcDataSource myBatisJdbcDataSource = DATASOURCE_MAP.get(name);
+        if (myBatisJdbcDataSource != null) {
+            myBatisJdbcDataSource.close();
+            DATASOURCE_MAP.remove(name);
+        }
+        return myBatisJdbcDataSource != null;
+    }
+
+    /**
+     * 删除所有数据源
+     */
+    @SneakyThrows
+    public void delAll() {
+        Iterator<Map.Entry<String, MyBatisJdbcDataSource>> iterator = DATASOURCE_MAP.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, MyBatisJdbcDataSource> entry = iterator.next();
+            entry.getValue().close();
+            iterator.remove();
+        }
+        defaultName = null;
+    }
+
+    /**
+     * 获取所有数据源名称
+     */
+    public Set<String> allNames() {
+        return DATASOURCE_MAP.keySet();
+    }
+
+    /**
+     * 获取数据源信息
+     *
+     * @param name 数据源名称
+     */
+    public JdbcInfo getInfo(String name) {
+        MyBatisJdbcDataSource myBatisJdbcDataSource = getDataSource(name);
+        return myBatisJdbcDataSource == null ? null : myBatisJdbcDataSource.getInfo();
+    }
+
+    /**
+     * 获取所有数据源信息
+     */
+    public Map<String, JdbcInfo> allInfos() {
+        Map<String, JdbcInfo> map = new HashMap<>(DATASOURCE_MAP.size());
+        for (Map.Entry<String, MyBatisJdbcDataSource> entry : DATASOURCE_MAP.entrySet()) {
+            String name = entry.getKey();
+            map.put(name, getInfo(name));
+        }
+        return map;
+    }
+
+    /**
+     * 获取数据源状态
+     *
+     * @param name 数据源名称
+     */
+    public JdbcDataSourceStatus getStatus(String name) {
+        MyBatisJdbcDataSource myBatisJdbcDataSource = getDataSource(name);
+        return myBatisJdbcDataSource == null ? null : myBatisJdbcDataSource.getStatus();
+    }
+
+    /**
+     * 获取数据源状态
+     */
+    public Map<String, JdbcDataSourceStatus> allStatus() {
+        Map<String, JdbcDataSourceStatus> map = new HashMap<>(DATASOURCE_MAP.size());
+        for (Map.Entry<String, MyBatisJdbcDataSource> entry : DATASOURCE_MAP.entrySet()) {
+            String name = entry.getKey();
+            map.put(name, getStatus(name));
+        }
+        return map;
+    }
 }
